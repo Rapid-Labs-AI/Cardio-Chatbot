d6903c777253e1b111157bd8492df54eb44f3e41
Haseeb-rapidAI <haseeb@rapidlabs.ai>
2024-05-07 20:21:48 +0500

    Updatd Cardiobot by adding Retrieval Chains

--- app/views.py
+++ app/views.py
@@ -1,89 +1,88 @@
-from django.shortcuts import render
-import re
 import os
-from langchain.vectorstores.chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from django.db import connection
-from .models import Chat_answers  
-
+import pdfplumber
+from django.shortcuts import render
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores.faiss import FAISS
+from langchain.vectorstores.base import VectorStoreRetriever
+from langchain.docstore.document import Document
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+import textwrap
 def home(request):
     return render(request, 'home.html')
 
 def chatbot(request):
-    CHROMA_PATH = "chroma"
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'fallback-api-key-if-none-found')
+    class PDFTextRetrieverMaker:
+    
+        @staticmethod
+        def extract_text_from_pdf(file_path):
+            text_content = []
+            with pdfplumber.open(file_path) as pdf:
+                for page in pdf.pages:
+                    extracted_text = page.extract_text()
+                    if extracted_text:  # Add text only if it is not None
+                        text_content.append(extracted_text)
+            return " ".join(text_content)
 
-    PROMPT_TEMPLATE = """
-    Hi! Thanks for reaching out. Let's look at the information I've gathered for you:
+        @classmethod
+        def generate_docs_from_file(cls, file_path: str,  max_length=2000) -> list[Document]:
+            full_text = cls.extract_text_from_pdf(file_path)
+            document_text = cls.extract_text_from_pdf(file_path)
+            chunks = textwrap.wrap(full_text, max_length, break_long_words=False, replace_whitespace=False)
+            documents = [Document(content=chunk, page_content=chunk, metadata={'source': file_path}) for chunk in chunks]
+            return documents
 
-    {context}
+        @classmethod
+        def generate_embeddings_for_docs(cls, documents: list[Document]) -> FAISS:
+            return FAISS.from_documents(documents, OpenAIEmbeddings())
 
-    Certainly, here’s how I can help with your question: {question}
-
-    I hope this helps! Feel free to ask more questions or clarify if you need further information.
-    """
-
-    GREETING_RESPONSES = [
-        "hello", "hi", "hey", "greetings", "good morning", "good afternoon", "good evening",
-        "can i ask you a question", "can i ask a question", "i have a question"
-    ]
-
-    def get_user_question():
-        """Prompt the user for a question and return it."""
-        return input("Hello! Do you have any query related to Cardiac health? ")
+        @classmethod
+        def make_retriever(cls, file_path: str) -> VectorStoreRetriever:
+            docs = cls.generate_docs_from_file(file_path)
+            faiss_storage = cls.generate_embeddings_for_docs(docs)
+            return faiss_storage.as_retriever(
+                search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.5}
+                )
 
 
-    def main():
+    class OpenAIDocumentAI:
+    
+        _prompt_template = '''
+    You are an Intelligent AI Chatbot. Your name is Cardiobot and you are helping humans to find answers related to Cardiac Health Issues.
+    You will only answer questions related to Cardiac health, Cardiovascular diseases, Heart disorders.
+    Use ONLY the following pieces of information provided. If the necessary information is absent, just say so.
+    ------
+    {summaries}
+    ------
+    
+    History:
+    {history}
+    Human: {question}
+    AI: '''
+    
+        def __init__(self, retriever: VectorStoreRetriever, verbose: bool = False):
+            llm = OpenAI(temperature=0, max_tokens=1000)
+            memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=200, input_key='question')
+            prompt = PromptTemplate.from_template(self._prompt_template)
+            self.conversation_retrieval_chain = RetrievalQAWithSourcesChain.from_chain_type(
+                llm=llm, retriever=retriever, chain_type_kwargs={'memory': memory, 'prompt': prompt, 'verbose': verbose})
+        
+        def ask(self, request: str) -> str:
+            return self.conversation_retrieval_chain.invoke({'question': request}, return_only_outputs=True)['answer']
+
+
+    if __name__ == '__main__':
+        if not load_dotenv():
+            raise RuntimeError('.env not loaded')
+        
+        datafile_path = (r"C:\Users\AP\Desktop\Chatbot\DATA\pdf1.pdf")
+            
+        retriever = PDFTextRetrieverMaker.make_retriever(datafile_path)
+        chatbot = OpenAIDocumentAI(retriever)
         while True:
-            query_text = get_user_question().lower()  
-            if query_text == "clear":                
-                os.system(query_text)
-                print("Thank you for using our service. Have a great day!")
-                break
-
-            parts = re.split(r'[?.!]\s*', query_text)
-            greeting_part = None
-            specific_question = None
-            for part in parts:
-                if any(greeting in part for greeting in GREETING_RESPONSES):
-                    greeting_part = part
-                else:
-                    if part.strip():
-                        specific_question = part
-
-            if greeting_part and not specific_question:
-                print("Hello! I'm here to help you with any questions you might have about mental health. What would you like to know more about?")
-                continue
-
-            embedding_function = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-            db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
-            results = db.similarity_search_with_relevance_scores(specific_question, k=3)
-            if len(results) == 0 or results[0][1] < 0.7:
-                print("I couldn’t find any information directly related to your question. Could you please provide more details or ask another question?")
-                continue
-
-            context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-            prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-            prompt = prompt_template.format(context=context_text, question=specific_question)
-
-            model = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
-            response_text = model.predict(prompt)
-            print(response_text)
-
-            save_chat_answer(response_text)
-            
-    def save_chat_answer(response_text):
-
-        sql = "INSERT INTO appname_chat_answers (Answer) VALUES (%s);"
-        
-        with connection.cursor() as cursor:
-        
-            cursor.execute(sql, [response_text])
-            
-
-    if __name__ == "__main__":
-        main()
-
-    return render(request, 'cardiobot.html')
+            request = input('Human: ')
+            response = chatbot.ask(request)
+            print(f'AI: {response}')
